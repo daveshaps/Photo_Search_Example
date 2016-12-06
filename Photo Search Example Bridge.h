3d9ffa242b25b3efc3b4c9e4870500b37cf90cfc
Wish Carr <wishcarr@wishs-macbook-pro.local>
2016-12-06 13:12:01 -0500

    Fixed AFNetworking. Now get an error for type any has no subscript members.

---
+++ Photo Search Example Bridge.h
@@ -0,0 +1,18 @@
+//
+//  Photo Search Example Bridge.h
+//  Photo Search Example
+//
+//  Created by Wish Carr on 12/5/16.
+//  Copyright © 2016 David Shapiro. All rights reserved.
+//
+
+#ifndef Photo_Search_Example_Bridge_h
+#define Photo_Search_Example_Bridge_h
+
+
+#endif /* Photo_Search_Example_Bridge_h */
+
+#import "AFNetworking.h"
+
+#import "UIKit+AFNetworking.h"
+//UIKit+AFNetworking/
--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchFlickrBy("dogs")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,62 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func searchFlickrBy(_ searchString: String) {
+        //calling Flickr API
+        let manager = AFHTTPSessionManager()
+        //question: how would you know what to put in the search parameters?
+        let searchParameters:[String:Any] = ["method": "flickr.photos.search",
+                                             "api_key": "YOUR_API_KEY_FROM_ABOVE",
+                                             "format": "json",
+                                             "nojsoncallback": 1,
+                                             "text": searchString,
+                                             "extras": "url_m",
+                                             "per_page": 5]
+        //this part is the same as in the web service example
+        manager.get("https://api.flickr.com/services/rest/",
+                    parameters: searchParameters,
+                    progress: nil,
+                    success: { (operation: URLSessionDataTask, responseObject:Any?) in
+                        if let responseObject = responseObject {
+                            print("Response: " + (responseObject as AnyObject).description)
+                            
+                            if let photos = responseObject["photos"] as? [String: AnyObject] {
+                                if let photoArray = photos["photo"] as? [[String: AnyObject]] {
+                                    
+                                    let imageWidth = self.view.frame.width
+                                    //set size of scrollView
+                                    self.scrollView.contentSize = CGSize(width: imageWidth, height: imageWidth * CGFloat(photoArray.count))
+                                    
+                                    //loop through phoroArray to extract photos
+                                    for (i,photoDictionary) in photoArray.enumerate() {
+                                        if let imageURLString = photoDictionary["url_m"] as? String {
+                                            
+                                            let imageView = UIImageView(frame: CGRect(x:0, y:imageWidth*CGFloat(i), width:imageWidth, height:imageWidth)) //created empty imageView
+                                            if let url = URL(string: imageURLString) {
+                                                imageView.setImageWith(url) //setImageWith() is from UIKit+AFNet and is an asynchronous method to download images in the background
+                                                self.scrollView.addSubview(imageView)
+                                            }
+                                        }
+                                    }
+                                }
+                                
+                            }
+                        }
+        }) { (operation:URLSessionDataTask?, error:Error) in
+            print("Error: " + error.localizedDescription)
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        searchBar.resignFirstResponder()
+        if let searchText = searchBar.text {
+            searchFlickrBy(searchText)
+        }
+    }
+    
+    
 }
 
